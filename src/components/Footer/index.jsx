1be33b2db38459fcbba343e0c493af168307ef10
NimishaVirani <nimishavirani54@gmail.com>
2021-10-27 00:37:51 +0530

    edit gitignore file

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,21 +3,14 @@ import {FooterContainer, ExternalLink} from './styles'
 
 const Footer = () => (
   <FooterContainer>
+    
     <ExternalLink
-      href="https://github.com/isabelxklee/winner-winner-chicken-dinner"
+      href="https://github.com/JayAkbari"
       target="_blank"
       rel="noopener noreferrer"
       className="footer-link"
     >
-      GitHub
-    </ExternalLink>
-    <ExternalLink
-      href="https://www.kleetime.com"
-      target="_blank"
-      rel="noopener noreferrer"
-      className="footer-link"
-    >
-      Made by Isabel K. Lee
+      Made by AJ
     </ExternalLink>
   </FooterContainer>
 )
